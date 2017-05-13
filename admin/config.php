9b1a652b85bc2bfa8eb378ff0c35dfae8fed9b47
demon_id <demon_id@itechs.com.ua>
2017-05-13 18:21:47 +0300

    ++

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://sad.lh/admin/');
+define('HTTP_CATALOG', 'http://sad.lh/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://sad.lh/admin/');
+define('HTTPS_CATALOG', 'http://sad.lh/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Projects/sources/sad/admin/');
+define('DIR_SYSTEM', 'D:/Projects/sources/sad/system/');
+define('DIR_IMAGE', 'D:/Projects/sources/sad/image/');
+define('DIR_LANGUAGE', 'D:/Projects/sources/sad/admin/language/');
+define('DIR_TEMPLATE', 'D:/Projects/sources/sad/admin/view/template/');
+define('DIR_CONFIG', 'D:/Projects/sources/sad/system/config/');
+define('DIR_CACHE', 'D:/Projects/sources/sad/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Projects/sources/sad/system/storage/download/');
+define('DIR_LOGS', 'D:/Projects/sources/sad/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Projects/sources/sad/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Projects/sources/sad/system/storage/upload/');
+define('DIR_CATALOG', 'D:/Projects/sources/sad/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sad');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://sad.lh/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://sad.lh/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Projects/sources/sad/catalog/');
+define('DIR_SYSTEM', 'D:/Projects/sources/sad/system/');
+define('DIR_IMAGE', 'D:/Projects/sources/sad/image/');
+define('DIR_LANGUAGE', 'D:/Projects/sources/sad/catalog/language/');
+define('DIR_TEMPLATE', 'D:/Projects/sources/sad/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/Projects/sources/sad/system/config/');
+define('DIR_CACHE', 'D:/Projects/sources/sad/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Projects/sources/sad/system/storage/download/');
+define('DIR_LOGS', 'D:/Projects/sources/sad/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Projects/sources/sad/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Projects/sources/sad/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sad');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
