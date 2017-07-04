0b7c6b44cddfc9dfb68398e0495829b52562f0e7
demon_id <demon_id@mail.ru>
2017-07-04 11:51:49 +0300

    ++

--- catalog/controller/checkout/checkout.php
+++ catalog/controller/checkout/checkout.php
@@ -1,6 +1,10 @@
 <?php
 class ControllerCheckoutCheckout extends Controller {
 	public function index() {
+
+		// close this page
+		$this->response->redirect($this->url->link('checkout/cart', '', true));
+
 		// Validate cart has products and has stock.
 		if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
 			$this->response->redirect($this->url->link('checkout/cart'));
