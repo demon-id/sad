cbfac777750dbcb60d93aff90d55f89c7f487693
demon_id <demon_id@mail.ru>
2017-08-31 18:23:36 +0300

    ++

--- catalog/controller/checkout/cart.php
+++ catalog/controller/checkout/cart.php
@@ -1,6 +1,30 @@
 <?php
 class ControllerCheckoutCart extends Controller {
 	public function index() {
+
+		// Validate cart has products and has stock.
+		if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
+			$this->response->redirect($this->url->link('checkout/cart'));
+		}
+
+		// Validate minimum quantity requirements.
+		$products = $this->cart->getProducts();
+
+		foreach ($products as $product) {
+			$product_total = 0;
+
+			foreach ($products as $product_2) {
+				if ($product_2['product_id'] == $product['product_id']) {
+					$product_total += $product_2['quantity'];
+				}
+			}
+
+			if ($product['minimum'] > $product_total) {
+				$this->response->redirect($this->url->link('checkout/cart'));
+			}
+		}
+
+
 		$this->load->language('checkout/cart');
 
 		$this->document->setTitle($this->language->get('heading_title'));
